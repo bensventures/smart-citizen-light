3125b78e5de8888c76d51cea07b10d2e22a1bb62
Benoit <benoit.gilloz@gmail.com>
2022-01-28 12:49:18 +0100

    Remove test keyword

--- src/components/device.tsx
+++ src/components/device.tsx
@@ -38,7 +38,7 @@ export default function Device(props: any) {
         <article className="device">
             <header>
                 <h1 className="device-name">
-                    {device.name} Test
+                    {device.name}
                 </h1>
                 <span className="battery">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-battery"><rect x="1" y="6" width="18" height="12" rx="2" ry="2"></rect><line x1="23" y1="13" x2="23" y2="11"></line></svg>
